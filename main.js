b267a85ede952f21b0f34d02f73849495c1701fd
Feoh258258 <65220078+Feoh258258@users.noreply.github.com>
2020-06-11 15:00:59 +0300

    Remove message time

--- main.js
+++ main.js
@@ -167,9 +167,9 @@ $.each(chatMessages, function(index, obj) {
   msgname = "." + obj.name;
   msginner = ".messageinner-" + obj.name;
   spinner = ".sp-" + obj.name;
-  if (obj.showTime == true) {
+  /*if (obj.showTime == true) {
     chatTimeString = "<span class='message-time'>" + obj.time + "</span>";
-  }
+  }*/
   $(".chat-message-list").append("<li class='message-" + obj.align + " " + obj.name + "' hidden><div class='sp-" + obj.name + "'><span class='spinme-" + obj.align + "'><div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div></span></div><div class='messageinner-" + obj.name + "' hidden><span class='message-text'>" + obj.msg + "</span>" + chatTimeString + "</div></li>");
   $(msgname).delay(chatDelay).fadeIn();
   $(spinner).delay(chatDelay2).hide(1);
